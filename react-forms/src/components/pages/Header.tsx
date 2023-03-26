5b1c51192e794bed103a03aa0bb12a0154a23682
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-03-26 13:57:42 +0300

    feat: add styles form and cardform

--- react-forms/src/components/pages/Header.tsx
+++ react-forms/src/components/pages/Header.tsx
@@ -19,8 +19,9 @@ export class Header extends Component<IHeaderProps> {
         <header className="header">
           <nav className="page-navigation">
             <div className="page-navigation-links">
-              <NavLink to="/">Start Page</NavLink>
+              <NavLink to="/">Start Wines</NavLink>
               <NavLink to="/about">About Page</NavLink>
+              <NavLink to="/form">Form Page</NavLink>
             </div>
             <h6 className="current-page">{namePage}</h6>
           </nav>
