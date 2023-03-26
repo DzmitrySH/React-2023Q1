a71ad20bee26c39c787fd863a2b7f07a87513d58
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-03-26 16:34:32 +0300

    feat: add images form

---
+++ react-forms/src/components/pages/forms/input/Images.tsx
@@ -0,0 +1,30 @@
+import React, { RefObject } from 'react';
+import { Component } from 'react';
+
+interface IinputImageProps {
+  valid: boolean;
+  imageRef: RefObject<HTMLInputElement>;
+  imageFile: File | null;
+}
+
+class Images extends Component<IinputImageProps> {
+  render() {
+    const { valid, imageRef, imageFile } = this.props;
+    return (
+      <div className="form-input">
+        <label htmlFor="image-input">
+          Image: {!valid && <span className="form-input-span-error">Error</span>}
+        </label>
+        <input
+          type="file"
+          accept="image/jpeg,image/png,image/gif"
+          id="image-input"
+          ref={imageRef}
+        />
+        {imageFile && <span>{imageFile.name}</span>}
+      </div>
+    );
+  }
+}
+
+export default Images;
