c0ae9b0649b4dd4e0495bd8f8ee39f417719ee4b
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-04-01 16:10:17 +0300

    feat: implement function wines-card

--- react-hooks/src/components/pages/WinesCard.tsx
+++ react-hooks/src/components/pages/WinesCard.tsx
@@ -1,43 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { IWinesProps } from 'components/interface/interface';
 import './WinesCard.css';
 
-interface IWinesCardState {
-  thumbnailError: boolean;
-}
+function WinesCard({ product }: IWinesProps) {
+  const [thumbnailError, setThumbnailError] = useState(false);
 
-class WinesCard extends Component<IWinesProps, IWinesCardState> {
-  constructor(props: IWinesProps) {
-    super(props);
-    this.state = {
-      thumbnailError: false,
-    };
-  }
-
-  handleThumbnailError = () => {
-    this.setState({ thumbnailError: true });
+  const handleThumbnailError = () => {
+    setThumbnailError(true);
   };
 
-  render() {
-    const { id, winery, wine, rating, reviews, price, location, image } = this.props.product;
-    const title = 'wine image';
+  const { id, winery, wine, rating, reviews, price, location, image } = product;
+  const title = 'wine image none';
 
-    return (
-      <div data-testid={`wines-card${id}`} className="wines-card" id={`wines-card${id}`}>
-        <div className="wines-card__image">
-          <img src={image} alt={title} onError={this.handleThumbnailError} />
-        </div>
-        <div className="wines-card__info">
-          <h2>Producer: {winery}</h2>
-          <p>Wine: {wine}</p>
-          <p>Price: {price}</p>
-          <p>Rating: {rating}</p>
-          <p>Reviews: {reviews}</p>
-          <p>Location: {location}</p>
-        </div>
+  return (
+    <div data-testid={`wines-card${id}`} className="wines-card" id={`wines-card${id}`}>
+      <div className="wines-card__image">
+        <img src={image} alt={title} onError={handleThumbnailError} />
       </div>
-    );
-  }
+      <div className="wines-card__info">
+        <h2>Producer: {winery}</h2>
+        <p>Wine: {wine}</p>
+        <p>Price: {price}</p>
+        <p>Rating: {rating}</p>
+        <p>Reviews: {reviews}</p>
+        <p>Location: {location}</p>
+      </div>
+    </div>
+  );
 }
 
 export default WinesCard;
