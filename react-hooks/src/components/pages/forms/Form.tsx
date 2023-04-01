60531a0643d7ff501bc462b102db9cccba46ab83
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-04-01 19:14:38 +0300

    feat: implement form & cardform function

--- react-hooks/src/components/pages/forms/Form.tsx
+++ react-hooks/src/components/pages/forms/Form.tsx
@@ -1,6 +1,6 @@
 import './Form.css';
 import { ICardWine } from 'components/interface/interface';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import CardForm from './CardForm';
 import InputForm from './ImputForm';
 
@@ -8,49 +8,31 @@ interface IFormWineProps {
   changeNamePage: (namePage: string) => void;
 }
 
-interface IFormWineState {
-  products: ICardWine[];
-}
+function Form({ changeNamePage }: IFormWineProps) {
+  useEffect(() => {
+    changeNamePage('Form');
+  });
 
-class Form extends Component<IFormWineProps, IFormWineState> {
-  constructor(props: IFormWineProps) {
-    super(props);
-    this.state = {
-      products: [],
-    };
-  }
-
-  componentDidMount() {
-    this.props.changeNamePage('Form Page');
-  }
-
-  changeProduct = (newProduct: ICardWine) => {
-    const { products } = this.state;
-    const newProducts = [...products, newProduct];
-
-    this.setState({
-      products: newProducts,
-    });
+  const [products, setWinProduct] = useState<ICardWine[]>([]);
+  const changeProduct = (newWine: ICardWine) => {
+    setWinProduct([...products, newWine]);
   };
 
-  render() {
-    const { products } = this.state;
-    return (
-      <div className="form-page">
-        <h3>Form page</h3>
-        <InputForm changeProduct={this.changeProduct} />
-        <div className="form-cards-container">
-          {products.length ? (
-            products.map((product) => {
-              return <CardForm key={product.id} product={{ ...product }} />;
-            })
-          ) : (
-            <div>No wines...</div>
-          )}
-        </div>
+  return (
+    <div className="form-page">
+      <h3>Form page</h3>
+      <InputForm changeProduct={changeProduct} />
+      <div className="form-cards-container">
+        {products.length ? (
+          products.map((product) => {
+            return <CardForm key={product.id} product={{ ...product }} />;
+          })
+        ) : (
+          <div>No wines...</div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Form;
