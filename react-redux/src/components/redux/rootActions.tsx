3673c74fd2e2ed47d746730bd5a723e93606ca3f
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-04-15 15:02:27 +0300

    feat: add action form & search slice

--- react-redux/src/components/redux/rootActions.tsx
+++ react-redux/src/components/redux/rootActions.tsx
@@ -1,1 +1,7 @@
-export const rootActions {};
+import { actions } from './services/formSlice';
+import { actions as searchActions } from './services/searchSlice';
+
+export const rootActions = {
+  ...actions,
+  ...searchActions,
+};
