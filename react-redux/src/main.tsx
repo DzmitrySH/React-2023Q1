5740c44deb2164862f9b854d44ea9b0c154cf3d7
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-04-15 14:53:35 +0300

    feat: add searchslice action

--- react-redux/src/main.tsx
+++ react-redux/src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
+import { store } from './components/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
-    <Provider store={undefined}}>
+    <Provider store={store}}>
       <App />
     </Provider>
   </BrowserRouter>
